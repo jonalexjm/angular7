3360c585593faedae886eeb8ad4ecd78a5e3ac42
john alexander jimenez morales <jhonalexjm@gmail.com>
2019-01-17 18:41:51 -0500

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { environment} from 'src/environments/environment';
 import { NgbModule  } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -16,5 +16,6 @@ export class TodoListComponent implements OnInit {
   clickAddTodo(){
     alert('hola');
   }
+  
 
 }
